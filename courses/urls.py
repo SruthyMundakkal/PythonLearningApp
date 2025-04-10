7d2bc2d6b3a376b6d7b72918d3c895d58451543d
SruthyMundakkal <sruthy228.1997@gmail.com>
2025-04-10 17:32:55 -0300

    added styles and retake button

--- courses/urls.py
+++ courses/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('lesson/<int:id>/', views.lesson_detail, name='lesson-detail'),
     path('quiz/', views.quiz, name='quiz'),
+    path('quiz/', views.quiz, name='quiz'),
     path('lesson/<int:lesson_id>/topics/', views.lesson_topics, name='lesson_topics'),
     path('topic/<int:topic_id>/details/', views.topic_detail, name='topic_detail'),
 ]
