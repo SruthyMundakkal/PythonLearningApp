052137d6e69dba7b51f59b54496b05a290876182
SruthyMundakkal <sruthy228.1997@gmail.com>
2025-04-11 04:21:38 -0300

    deployment files added

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnpy.settings')
+
+# Create the WSGI application that Vercel will use
+application = get_wsgi_application()
